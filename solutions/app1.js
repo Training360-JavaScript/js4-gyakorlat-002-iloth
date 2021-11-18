134ad871a8d2178c56d0420525ab1619e1f7720a
iloth <loth.istvan@hotmail.com>
2021-11-18 20:39:29 +0100

    Solution

--- solutions/app1.js
+++ solutions/app1.js
@@ -2,29 +2,29 @@ let serverData = null;
 const getServerData = () => serverData;
 
 /**
- * TODO: a startFetch függvény futtasson egy fetch kérést. 
+ * TODO: a startFetch függvény futtasson egy fetch kérést.
  * Javítsd a hibás paraméteket a fetch függvény meghívásánál!
  * CSAK A 22. SORBAN DOLGOZZ!
- * 
+ *
  * PÉLDÁK: https://developer.mozilla.org/en-US/docs/Web/API/fetch#examples
- * 
+ *
  * LEÍRÁS: A függvény a kapott paraméterek alapján indít egy fetch kérést.
- * Miután a válasz megérkezett, az első függvényben, amelyet az első .then-ben 
+ * Miután a válasz megérkezett, az első függvényben, amelyet az első .then-ben
  * átadunk, parse-oljuk a JSON-adatokat.
- * Majd a második .then-ben a parse-olt adatokkal felülírjuk a serverData 
+ * Majd a második .then-ben a parse-olt adatokkal felülírjuk a serverData
  * változót.
- * Hiba esetén a .catch metódusban átadott függvény fut le, amely kiírja a 
+ * Hiba esetén a .catch metódusban átadott függvény fut le, amely kiírja a
  * megfelelő hibaüzenetet a console.log segítségével.
  * @param {string} method a kérés HTTP-metódusa, lehet GET|POST|DELETE|PUT|PATCH
  * @param {string} url a távoli erőforrás címe, ahonnan lekérjük az adatokat
  */
 const startFetch = (method = 'GET', url = '') => {
-    fetch('', {  })
-        .then(response => response.json())
-        .then(data => serverData = data)
-        .catch(
-            () => console.log(`Error: ${url} is not found!`),
-        );
+  fetch(url, { method })
+    .then((response) => response.json())
+    .then((data) => { serverData = data; })
+    .catch(
+      () => console.log(`Error: ${url} is not found!`),
+    );
 };
 
 /**
@@ -32,6 +32,6 @@ const startFetch = (method = 'GET', url = '') => {
  * CSAK A 35. SORBAN DOLGOZZ!
  */
 export {
-    
-    getServerData,
-}
+  startFetch,
+  getServerData,
+};
--- solutions/app2.js
+++ solutions/app2.js
@@ -3,28 +3,28 @@ const getUserData = () => userData;
 
 /**
  * TODO: a getUsers függvény futtasson egy fetch kérést.
- * Javítsd a hibás vagy hiányzó paramétereket a fetch függvény meghívásánál 
+ * Javítsd a hibás vagy hiányzó paramétereket a fetch függvény meghívásánál
  * és a kapott adatok parse-olásánál.
  * CSAK A 22-23. SOROKBAN DOLGOZZ!
- * 
+ *
  * PÉLDÁK: https://developer.mozilla.org/en-US/docs/Web/API/fetch#examples
- * 
+ *
  * LEÍRÁS: A függvény a kapott paraméterek alapján indít egy fetch kérést.
- * Miután a válasz megérkezett, az első függvényben, amelyet az első .then-ben 
+ * Miután a válasz megérkezett, az első függvényben, amelyet az első .then-ben
  * átadunk, parse-oljuk a JSON-adatokat.
- * Majd a második .then-ben a parse-olt adatokkal felülírjuk a userData 
+ * Majd a második .then-ben a parse-olt adatokkal felülírjuk a userData
  * változót.
- * Hiba esetén a .catch metódusban átadott függvény fut le, amely kiírja a 
+ * Hiba esetén a .catch metódusban átadott függvény fut le, amely kiírja a
  * megfelelő hibaüzenetet a console.log segítségével.
  * @param {string} url a távoli erőforrás címe, ahonnan lekérjük az adatokat
  */
 const getUsers = (url = '') => {
-    fetch('')
-        .then(() => {})
-        .then(data => userData = data)
-        .catch(
-            () => console.log(`Error: ${url} is not found!`),
-        );
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => { userData = data; })
+    .catch(
+      () => console.log(`Error: ${url} is not found!`),
+    );
 };
 
 /**
@@ -32,6 +32,6 @@ const getUsers = (url = '') => {
  * CSAK A 35. SORBAN DOLGOZZ!
  */
 export {
-    
-    getUserData,
-}
+  getUsers,
+  getUserData,
+};
